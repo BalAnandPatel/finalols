b59191a89d9a30e1b34be08d25cf144cb6f38692
Vikas7905 <vikas051743@gmail.com>
2024-10-29 17:06:04 +0530

    changes on seller>update-address and update personal information

--- sellar/update-personal.php
+++ sellar/update-personal.php
@@ -47,38 +47,99 @@ $result = json_decode($response);
 					<div class="content">
 						<div class="module">
 							<div class="module-head">
-								<h3>Update Address</h3>
+								<h3>Update Personal Info</h3>
 							</div>
 							<div class="module-body">
 								<form class="form-horizontal row-fluid" name="insertproduct" method="post" enctype="multipart/form-data" action="action/insertSellarPost.php">
+									<div class="control-group">
+										<label class="control-label" for="basicinput">Name</label>
+										<div class="controls">
+											<input type="text" name="sellarName" placeholder="Name" class="span8 tip" required>
+										</div>
+									</div>
+									<div class="control-group">
+										<label class="control-label" for="basicinput">Shop Title</label>
+										<div class="controls">
+											<input type="text" name="counterName" placeholder="Counter Name" class="span8 tip" required>
+										</div>
+									</div>
+
 
 									<div class="control-group">
-										<label class="control-label" for="basicinput">Adress</label>
+										<label class="control-label" for="basicinput">phone No</label>
 										<div class="controls">
-											<input type="text" name="address" placeholder="Address" class="span8 tip" required>
+											<input type="text" name="phoneNo" placeholder="Phone No" class="span8 tip" required>
+										</div>
+									</div>
+
+									<div class="control-group">
+										<label class="control-label" for="basicinput">Email Id</label>
+										<div class="controls">
+											<input type="text" name="email" placeholder="Enter Email Id" class="span8 tip" required>
 										</div>
 									</div>
 									<div class="control-group">
-										<label class="control-label" for="basicinput">Pin</label>
+										<label class="control-label" for="basicinput">Deposit Amount</label>
 										<div class="controls">
-											<input type="text" name="pin" placeholder="Pin" class="span8 tip" required>
+											<input type="text" name="depositAmount" placeholder="Deposit Amount" class="span8 tip" required>
 										</div>
 									</div>
 									<div class="control-group">
-										<label class="control-label" for="basicinput">City</label>
+										<label class="control-label" for="basicinput">Password</label>
 										<div class="controls">
-											<input type="text" name="city" placeholder="City" class="span8 tip" required>
+											<input type="text" name="password" placeholder="password" class="span8 tip" required>
 										</div>
 									</div>
 									<div class="control-group">
+										<label class="control-label" for="basicinput">Reg Fee</label>
+										<div class="controls">
+											<input type="text" name="regFee" placeholder="Reg Fee" class="span8 tip" required>
+										</div>
+									</div>
+
+
+									<div class="control-group">
+										<label class="control-label" for="basicinput">Gst</label>
+										<div class="controls">
+											<input type="text" name="gst" placeholder="Enter GST NO" class="span8 tip" required>
+										</div>
+									</div>
+
+									<div class="control-group">
+										<label class="control-label" for="basicinput">Aadhar</label>
+										<div class="controls">
+											<input type="text" name="aadhar" placeholder="Adhar no" class="span8 tip" required>
+										</div>
+									</div>
+									<div class="control-group">
+										<label class="control-label" for="basicinput">Pan</label>
+										<div class="controls">
+											<input type="text" name="pan" placeholder="pan Card" class="span8 tip" required>
+										</div>
+									</div>
+
+									<div class="control-group">
+										<label class="control-label" for="basicinput">Owner Image</label>
+										<div class="controls">
+											<input type="file" name="upload" placeholder="counter Image" class="span8 tip" required>
+										</div>
+									</div>
+									<div class="control-group">
+										<label class="control-label" for="basicinput">Shop Image</label>
+										<div class="controls">
+											<input type="file" name="shopupload" placeholder="shop Image" class="span8 tip" required>
+										</div>
+									</div>
+								 <div class="control-group">
 										<div class="controls">
 											<button type="submit" name="submit" class="btn">Update</button>
 										</div>
 									</div>
-								</form>
-							</div>
+                                </form>
+</div>
+</div>
 
-						</div>
+
 						
 					</div><!--/.content-->
 				</div><!--/.span9-->
